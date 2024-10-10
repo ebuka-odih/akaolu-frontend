dcad5635b26d80d9dfb8c212ac5ef31a28839eb4
Gnosis <emmagab38@gmail.com>
2024-10-10 21:23:29 +0100

    setup folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 import Image from "next/image";
+import logoImg from '@/public/images/logo.png'
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/public/images/2.png"
+          className=""
+          src={logoImg}
           alt="OLU.js logo"
-          width={200}
-          height={150}
+          width={230}
+          height={200}
           priority
         />
        
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-         <h2>OLU</h2>
-        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
        
